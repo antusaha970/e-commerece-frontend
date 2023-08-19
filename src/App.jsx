67342dda5475900602a0eaa776b345441a6a912a
Antu Saha <antusaha970@gmail.com>
2023-08-19 22:07:55 +0600

    added navbar productlist and auth components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+  return (
+    <>
+      <SignUpPage />
+    </>
+  );
+}
+
+export default App;
