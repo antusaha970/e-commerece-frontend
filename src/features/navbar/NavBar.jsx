e00459946ac35f3b438e497935421abf61df94e1
Antu Saha <antusaha970@gmail.com>
2023-09-16 02:09:05 +0600

    changed navbar ui

--- src/features/navbar/NavBar.jsx
+++ src/features/navbar/NavBar.jsx
@@ -1,5 +1,5 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+// import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   ShoppingCartIcon,
@@ -9,232 +9,165 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCartItems } from "../cart/cartSlice";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://t3.ftcdn.net/jpg/05/16/27/58/240_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg",
-};
-const navigation = [
-  { name: "Home", href: "/", current: true },
-  { name: "Profile", href: "/user-profile", current: false },
-];
-const userNavigation = [
-  { name: "My Profile", link: "/user-profile" },
-  { name: "My Orders", link: "/user-orders" },
-  { name: "Sign out", link: "/logout" },
-];
+import { Menu, X, ChevronDown, ChevronRight } from "lucide-react";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+const menuItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "Profile",
+    href: "/user-profile",
+  },
+];
 
 // eslint-disable-next-line react/prop-types
 const NavBar = ({ children }) => {
   const cartItems = useSelector(selectCartItems);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
-          {({ open }) => (
-            <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Link to="/">
-                        <img
-                          className="h-10 w-full"
-                          src="/logo.png"
-                          alt="Your Company"
-                        />
-                      </Link>
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <Link
-                            key={item.name}
-                            to={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                              "rounded-md px-3 py-2 text-sm font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
+        <div className="relative w-full bg-white">
+          <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+            <div className="inline-flex items-center space-x-2">
+              <Link to={"/"}>
+                <img
+                  className="h-10 w-full"
+                  src="/logo.png"
+                  alt="Your Company"
+                />
+              </Link>
+            </div>
+            <div className="hidden lg:block">
+              <ul className="ml-12 inline-flex space-x-8">
+                {menuItems.map((item) => (
+                  <li key={item.name}>
+                    <Link
+                      to={item.href}
+                      className="inline-flex items-center text-sm font-semibold text-gray-800 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+                <Link
+                  to={"/cart"}
+                  className="inline-flex items-center text-sm font-semibold text-gray-800 hover:text-gray-900"
+                >
+                  cart&nbsp;&nbsp;
+                  <ShoppingCartIcon
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  />{" "}
+                  {cartItems.length > 0 && (
+                    <span className="font-bold -mt-2 ">{cartItems.length}</span>
+                  )}
+                </Link>
+              </ul>
+            </div>
+            <div className="flex grow justify-end">
+              <input
+                className="flex h-10 w-[250px] rounded-md bg-gray-100 px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                type="text"
+                placeholder="Serach"
+              ></input>
+            </div>
+            <div className="ml-2 mt-2 hidden lg:block">
+              <Link to={"/user-profile"}>
+                <span className="relative inline-block">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src="https://t3.ftcdn.net/jpg/05/16/27/58/240_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+                    alt="user"
+                  />
+                  <span className="absolute bottom-0 right-0 block h-2.5 w-2.5 rounded-full bg-green-600 ring-2 ring-white"></span>
+                </span>
+              </Link>
+            </div>
+            <div className="ml-2 lg:hidden">
+              <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
+            </div>
+            {isMenuOpen && (
+              <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+                <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                  <div className="px-5 pb-6 pt-5">
+                    <div className="flex items-center justify-between">
+                      <div className="inline-flex items-center space-x-2">
+                        <Link to={"/"}>
+                          <img
+                            className="h-10 w-full"
+                            src="/logo.png"
+                            alt="Your Company"
+                          />
+                        </Link>
                       </div>
-                    </div>
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <Link to="/cart">
+                      <div className="-mr-2">
                         <button
                           type="button"
-                          className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          onClick={toggleMenu}
+                          className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                         >
-                          <span className="absolute -inset-1.5" />
-                          <span className="sr-only">View notifications</span>
+                          <span className="sr-only">Close menu</span>
+                          <X className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                    <div className="mt-6">
+                      <nav className="grid gap-y-4">
+                        {menuItems.map((item) => (
+                          <li key={item.name}>
+                            <Link
+                              to={item.href}
+                              className="inline-flex items-center text-sm font-semibold text-gray-800 hover:text-gray-900"
+                            >
+                              {item.name}
+                            </Link>
+                          </li>
+                        ))}
+                        <Link
+                          to={"/cart"}
+                          className="inline-flex items-center text-sm font-semibold text-gray-800 hover:text-gray-900"
+                        >
+                          cart&nbsp;&nbsp;
                           <ShoppingCartIcon
                             className="h-6 w-6"
                             aria-hidden="true"
-                          />
-                        </button>
-                      </Link>
-                      {cartItems.length > 0 && (
-                        <span className="inline-flex items-center mr-1 mb-7 rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                          {cartItems.length}
-                        </span>
-                      )}
-
-                      {/* Profile dropdown */}
-                      <Menu as="div" className="relative ml-3">
-                        <div>
-                          <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
-                          </Menu.Button>
-                        </div>
-                        <Transition
-                          as={Fragment}
-                          enter="transition ease-out duration-100"
-                          enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scale-100"
-                          leave="transition ease-in duration-75"
-                          leaveFrom="transform opacity-100 scale-100"
-                          leaveTo="transform opacity-0 scale-95"
-                        >
-                          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
-                                  <Link
-                                    to={item.link}
-                                    className={classNames(
-                                      active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
-                                    )}
-                                  >
-                                    {item.name}
-                                  </Link>
-                                )}
-                              </Menu.Item>
-                            ))}
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
+                          />{" "}
+                          {cartItems.length > 0 && (
+                            <span className="font-bold -mt-2 ">
+                              {cartItems.length}
+                            </span>
+                          )}
+                        </Link>
+                      </nav>
                     </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-0.5" />
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <Bars3Icon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
+                    <div className="ml-3 mt-4 flex items-center space-x-2">
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src="https://t3.ftcdn.net/jpg/05/16/27/58/240_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+                        alt="user"
+                      />
+                      <span className="flex flex-col">
+                        <Link
+                          to={"/user-profile"}
+                          className="text-sm font-medium text-gray-900"
+                        >
+                          User
+                        </Link>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-
-              <Disclosure.Panel className="md:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-                <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        {user.name}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {user.email}
-                      </div>
-                    </div>
-                    <Link to="/cart">
-                      <button
-                        type="button"
-                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <ShoppingCartIcon
-                          className="h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </button>
-                    </Link>
-                    {cartItems.length > 0 && (
-                      <span className="inline-flex items-center mr-1 mb-7 rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                        {cartItems.length}
-                      </span>
-                    )}
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                      >
-                        {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div>
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-
-        {/* <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
+            )}
           </div>
-        </header> */}
+        </div>
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             {children}
